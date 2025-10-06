23c1cc94c10c2fefaf6e2624f3d7a34d5bbb3b3b
Niukalo <ivannukalo1@gmail.com>
2025-10-06 08:56:52 +0300

    homework

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,59 @@
-// // Завдання на змінні:
+// // // Завдання на змінні:
 
-let a = 5;
-let b = 3;
-console.log(a * b);
+// let a = 5;
+// let b = 3;
+// console.log(a * b);
 
-let c = 10;
-let d = 2;
-console.log(c / d);
+// let c = 10;
+// let d = 2;
+// console.log(c / d);
 
-let e = 7;
-let f = 8;
-console.log(e + f);
+// let e = 7;
+// let f = 8;
+// console.log(e + f);
 
-let num1 = 11;
-let bool = true;
-let text = "hello world";
-let strNum = "100";
+// let num1 = 11;
+// let bool = true;
+// let text = "hello world";
+// let strNum = "100";
 
-console.log(num1, bool, text, strNum);
+// console.log(num1, bool, text, strNum);
 
-let num = 1;
-num += 11;
-num -= 11;
-num *= 11;
-num /= 11;
-num++;
-num--;
+// let num = 1;
+// num += 11;
+// num -= 11;
+// num *= 11;
+// num /= 11;
+// num++;
+// num--;
 
-// // Завдання на введення/виведення:
+// // // Завдання на введення/виведення:
 
-// // let number = prompt("Введіть число:");
-// // let square = number ** 2;
-// // alert("Квадрат числа: " + square);
+// // // let number = prompt("Введіть число:");
+// // // let square = number ** 2;
+// // // alert("Квадрат числа: " + square);
 
-// // let num1 = parseFloat(prompt("Введіть перше число:"));
-// // let num2 = parseFloat(prompt("Введіть друге число:"));
-// // let average = (num1 + num2) / 2;
-// // alert("Середнє арифметичне: " + average);
+// // // let num1 = parseFloat(prompt("Введіть перше число:"));
+// // // let num2 = parseFloat(prompt("Введіть друге число:"));
+// // // let average = (num1 + num2) / 2;
+// // // alert("Середнє арифметичне: " + average);
 
-// // let minutes = prompt("Введіть кількість хвилин:");
-// // let seconds = minutes * 60;
-// // alert("Це " + seconds + " секунд.");
+// // // let minutes = prompt("Введіть кількість хвилин:");
+// // // let seconds = minutes * 60;
+// // // alert("Це " + seconds + " секунд.");
 
-// let greeting = "Hello, ";
-// let userName = prompt("Як тебе звати?");
-// alert(greeting + userName + "!");
+// // let greeting = "Hello, ";
+// // let userName = prompt("Як тебе звати?");
+// // alert(greeting + userName + "!");
 
+const isLogged = false;
 
+if (isLogged) {
+    console.log ('hello');
+    console.log ('Вітаємо на сайті!');
+} else {
+    console.log ('again')
+}
 
 
 
